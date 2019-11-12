9dc3026736b366bd8e1ec686c4e59d8634a03774
nabest-vn <1261031552@qq.com>
2019-11-12 09:55:59 +0800

    test17

---
+++ leetcode-php/leetcode-php/suanfa/test17.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenzhe
+ * Date: 2019/11/12
+ * Time: 9:25 上午
+ */
+
+class Solution17
+{
+    public function letterCombinations($digits)
+    {
+        if(! preg_match('/^[2-9]+$/', $digits)){
+            return [];
+        }
+        $len = strlen($digits);
+        while
+    }
+    function getArray($str, $arr)
+    {
+        if(empty($arr)){
+
+        }
+    }
+}
